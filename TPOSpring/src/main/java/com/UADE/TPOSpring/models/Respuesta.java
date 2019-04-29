2bcf4fca45001ef05e8cd1368412d43c6356a30b
Ana Martinez Saucedo <anmartinez@uade.edu.ar>
2019-04-29 09:38:12 -0300

    Cambio a español de  clase

---
+++ TPOSpring/src/main/java/com/UADE/TPOSpring/models/Respuesta.java
@@ -0,0 +1,32 @@
+package ar.edu.uade.models;
+
+public class Respuesta {
+	private Boolean estado;
+	private String mensaje;
+	private String datos;
+
+	public Boolean getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Boolean status) {
+		this.estado = status;
+	}
+
+	public String getMensaje() {
+		return mensaje;
+	}
+
+	public void setMensaje(String message) {
+		this.mensaje = message;
+	}
+
+	public String getDatos() {
+		return datos;
+	}
+
+	public void setDatos(String data) {
+		this.datos = data;
+	}
+
+}
